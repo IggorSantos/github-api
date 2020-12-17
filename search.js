5882320344ab6f77a1f34f5ad3680abb8ab3f8ac
Igor Santos <igor.sagitarius@gmail.com>
2020-12-17 10:31:12 -0300

    Tratamento de erros quando o usuario não informa o valor

--- search.js
+++ search.js
@@ -1,20 +1,25 @@
 async function search(){
   const userSearched = document.getElementById('user').value
-  const responseUser = await fetch(`https://api.github.com/users/${userSearched}`)
-     .then((respostaDoServer) => {
-       if(respostaDoServer.ok){
-         return respostaDoServer.json()
-       }
-       else{
-         document.getElementById('resName').innerHTML = ("Não encontrado")
-         document.getElementById('resAvatar').src = ("https://via.placeholder.com/150/771796")
-         document.getElementById('resBio').innerHTML = ""
-         throw new Error("Não encontrado")
-       }
-     })
-     .then((respostaEmObjeto) => respostaEmObjeto)
-   document.getElementById('resName').innerHTML = responseUser.login
-   document.getElementById('resAvatar').src = responseUser.avatar_url
-   document.getElementById('resBio').innerHTML = responseUser.bio
-
+  if(userSearched === ""){
+    document.getElementById('resName').innerHTML = ("Digite um nome")
+    document.getElementById('resAvatar').src = ("https://via.placeholder.com/150/771796")
+    document.getElementById('resBio').innerHTML = ""
+  }else {
+    const responseUser = await fetch(`https://api.github.com/users/${userSearched}`)
+       .then((respostaDoServer) => {
+         if(respostaDoServer.ok){
+           return respostaDoServer.json()
+         }
+         else{
+           document.getElementById('resName').innerHTML = ("Não encontrado")
+           document.getElementById('resAvatar').src = ("https://via.placeholder.com/150/771796")
+           document.getElementById('resBio').innerHTML = ""
+           throw new Error("Não encontrado")
+         }
+       })
+       .then((respostaEmObjeto) => respostaEmObjeto)
+     document.getElementById('resName').innerHTML = responseUser.login
+     document.getElementById('resAvatar').src = responseUser.avatar_url
+     document.getElementById('resBio').innerHTML = responseUser.bio
+  }
 }
