be609bc70f37ab1c8bae6a704e19184610a944dc
Igor Santos <igor.sagitarius@gmail.com>
2021-11-12 15:23:06 -0300

    Modificação da função que busca na api para await

--- search.js
+++ search.js
@@ -1,14 +1,14 @@
 async function search(){
   const userSearched = document.getElementById('user').value
+  let responseJson = ''
   if(userSearched === ""){
     document.getElementById('resName').innerHTML = ("Digite um nome")
     document.getElementById('resAvatar').src = ("https://via.placeholder.com/150/771796")
     document.getElementById('resBio').innerHTML = ""
   }else {
     const responseUser = await fetch(`https://api.github.com/users/${userSearched}`)
-       .then((respostaDoServer) => {
-         if(respostaDoServer.ok){
-           return respostaDoServer.json()
+    if(responseUser.ok){
+           responseJson = await responseUser.json()
          }
          else{
            document.getElementById('resName').innerHTML = ("Não encontrado")
@@ -16,9 +16,8 @@ async function search(){
            document.getElementById('resBio').innerHTML = ""
            throw new Error("Não encontrado")
          }
-       })
-     document.getElementById('resName').innerHTML = responseUser.login
-     document.getElementById('resAvatar').src = responseUser.avatar_url
-     document.getElementById('resBio').innerHTML = responseUser.bio
+     document.getElementById('resName').innerHTML = responseJson.login
+     document.getElementById('resAvatar').src = responseJson.avatar_url
+     document.getElementById('resBio').innerHTML = responseJson.bio
   }
 }
